18241417cc664610c9444fd7b06940bd391f62fb
Alice Sonolet <alice.sonolet@gmail.com>
2024-04-24 00:27:26 +0200

    Add base template for egui

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Reito
-Reito Rendering engine
+# Raito
+Raito Rendering engine
+
+## Dependencies
+
+The GUI is created with [egui](https://github.com/emilk/egui) and [eframe](https://github.com/emilk/egui/tree/master/crates/eframe) using the [eframe template](https://github.com/emilk/eframe_template/)
+
+## Run
+
+1. [Install rust](https://www.rust-lang.org/tools/install)
+2. Install eframe dependencies :
+  - Linux : `sudo apt-get install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libxkbcommon-dev libssl-dev`
+  - Fedora : `sudo dnf install clang clang-devel clang-tools-extra libxkbcommon-devel pkg-config openssl-devel libxcb-devel gtk3-devel atk fontconfig-devel`
+3. Build and run : `cargo run --release`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+fn main() -> eframe::Result<()> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0])
+            .with_icon(
+                // NOTE: Adding an icon is optional
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                    .expect("Failed to load icon"),
+            ),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Raito Render",
+        native_options,
+        Box::new(|cc| Box::new(raito::RaitoRenderApp::new(cc))),
+    )
 }
